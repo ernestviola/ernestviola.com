709a4a99ea92b911783f348d63345adf417c3914
Ernest Viola <ernestviola@ernests-mbp.attlocal.net>
2022-06-15 22:59:26 -0700

    Fixed path

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Home from './components/home/Home'
 import NotFound from './components/notfound/NotFound'
 import Projects from './components/project/Projects';
 import Login from './components/authorization/Login';
-import Logout from './components/authorization/Signout';
+import Logout from './components/authorization/Logout';
 import CheckUser from './components/authorization/CheckUser';
 
 
