3094a15e6a0f26c7503b7612a76cc1756c0e4398
ernestviola <ernest.viol@gmail.com>
2022-06-25 21:42:07 -0700

    added link to projects on homepage

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -40,7 +40,7 @@ const Home = () => {
       <div className='page'>
             
               <h1>About Me</h1>
-              <p>First of all, welcome to my website! I am an aspiring <b>software engineer</b> and an aspiring old person, by that I really mean an aspiring <b>storyteller</b>. I hope that with this blog I can showcase some of my projects as well as add a bit of personality to some of the things I experience in life.</p>
+              <p>First of all, welcome to my website! I am an aspiring <b>software engineer</b> and an aspiring old person, by that I really mean an aspiring <b>storyteller</b>. I hope that with this blog I can showcase some of my <Link to='/projects'>projects</Link> as well as add a bit of personality to some of the things I experience in life.</p>
               
               <p>My dad, also named Ernest, always said to leave a place better than how you found it. That's my goal in life, to make an impact on the world around me and to be a positive influence.</p>
 
