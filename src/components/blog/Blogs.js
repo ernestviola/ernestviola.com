0f6670d2f3c42fea2c6d83dfe76284fd35b44146
ernestviola <ernest.viol@gmail.com>
2022-07-18 22:53:55 -0400

    Able to list blogs

--- src/components/blog/Blogs.js
+++ src/components/blog/Blogs.js
@@ -1,44 +1,34 @@
 import React, {useState,useEffect} from 'react';
+
+import {API, Auth} from 'aws-amplify';
 import axios from "axios";
 
 import Lists from '../List';
 import SearchBar from '../SearchBar';
 
 const Blogs = () => {
-    const [blogs, setBlogs] = useState([]);
-    const [users, setUsers] = useState([]);
-    const [store, setStore] = useState([]);
-     
-    const getUsers = () => {
-        axios
-          .get("https://randomuser.me/api/?results=10&inc=name,registered&nat=fr")
-          .then((response) => {
-            const newData = response.data.results.map((result) => ({
-              name: `${result.name.first} ${result.name.last}`,
-              id: result.registered
-            }));
-            setUsers(newData);
-            // setStore(newData);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      };
+    const [blogs, setBlogs] = useState([])
 
-    const filterNames = (event) => {
-      const filteredData = store.filter((item) =>
-          item.name.toLowerCase().includes(event.target.value.toLowerCase())
-      );
-      setUsers(filteredData);
-    };
+    const fetchBlogs = async () => {
+      const resData = await API.get('blogsApi','/blogs/uuid')
+      setBlogs(resData)
+    }
      
     useEffect(() => {
-      getUsers();
+      fetchBlogs()
     }, []);
    
     return (
         <div className="page">
             <h1>Blogs</h1>
+            <ul>
+              {blogs.map(blog => 
+                <li key= {blog.uuid}>
+                  {blog.title}
+                  {blog.content}
+                </li>
+              )}
+            </ul>
         </div>
     );
 }
