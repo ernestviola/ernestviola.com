d4108327bd52348ffa4647c72f9e243637cebbcb
ernestviola <ernest.viol@gmail.com>
2022-10-13 22:49:07 -0400

    Added inline and sidebar editors

--- src/components/CustomEditor.js
+++ src/components/CustomEditor.js
@@ -4,15 +4,33 @@ import { EditorState, RichUtils, convertToRaw, convertFromRaw } from "draft-js";
 import Editor from "@draft-js-plugins/editor";
 import createEmojiPlugin from "@draft-js-plugins/emoji";
 import createInlineToolbarPlugin from '@draft-js-plugins/inline-toolbar';
+import createSideToolbarPlugin from '@draft-js-plugins/side-toolbar';
 
-import '../styles/CustomEditor.css'
+// import {
+//     ItalicButton,
+//     BoldButton,
+//     UnderlineButton,
+//     CodeButton
+//   } from "draft-js-buttons";
+
+
+import '../styles/CustomEditor.css';
 import '@aws-amplify/ui-react/styles.css';
+import '@draft-js-plugins/inline-toolbar/lib/plugin.css';
+import '@draft-js-plugins/inline-toolbar/lib/plugin.css';
 
 const CustomEditor = (props) => {
     const [editorState, setEditorState] = useState(props.contentState ? EditorState.createWithContent(convertFromRaw(props.contentState)) : EditorState.createEmpty())
     const [emojiPlugin, setEmojiPlugin] = useState(createEmojiPlugin())
+
     const [inlineToolbarPlugin, setInlineToolbarPlugin] = useState(createInlineToolbarPlugin())
+    const [sideToolbarPlugin, setsideToolbarPlugin] = useState(createSideToolbarPlugin())
+
+    const { SideToolbar } = sideToolbarPlugin
     const { EmojiSuggestions } = emojiPlugin
+    const { InlineToolbar } = inlineToolbarPlugin
+    
+
 
     const onChange = (editorState) => {
         setEditorState(editorState)
@@ -44,10 +62,13 @@ const CustomEditor = (props) => {
                 editorState={editorState}
                 handleKeyCommand={handleKeyCommand}
                 onChange={onChange}
-                plugins={[emojiPlugin,inlineToolbarPlugin]}
+                plugins={[emojiPlugin,inlineToolbarPlugin,sideToolbarPlugin]}
                 readOnly={props.readOnly}
             />
+            <SideToolbar />
+            <InlineToolbar />
             <EmojiSuggestions warn />
+
         </div>
     )
 }
