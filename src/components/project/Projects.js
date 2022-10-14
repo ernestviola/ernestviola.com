cd671f285cdf73d775efef244ecbc77a61ebc71d
ernestviola <ernest.viol@gmail.com>
2022-10-14 15:23:37 -0400

    Rename header in projects from blogs to projects

--- src/components/project/Projects.js
+++ src/components/project/Projects.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const Projects = () => {
   return (
     <div className='page'>
-      <h1>Blogs</h1>
-      <ul className='blogs'>
+      <h1>Projects</h1>
+      <ul className='projects'>
         <li key={1}>
           <p><a href='https://main.d2x5veo5ef99gu.amplifyapp.com/' target={'_blank'}>Algorithm Visualizer</a> - A visualization app for demonstrating knowledge of algorithms.</p>
         </li>
