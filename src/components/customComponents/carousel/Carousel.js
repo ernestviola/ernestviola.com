325622dfd0eeba50f1a4cab1bb9df1b1d0ca53e7
ernestviola <ernest.viol@gmail.com>
2022-12-20 21:23:49 -0500

    Created Carousel

--- src/components/customComponents/carousel/Carousel.js
+++ src/components/customComponents/carousel/Carousel.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
-const Carousel = () => {
+// https://medium.com/tinyso/how-to-create-the-responsive-and-swipeable-carousel-slider-component-in-react-99f433364aa0
+
+const CarouselItem = ({children,width}) => {
   return (
-    <div>Carousel</div>
+    <div className='carousel-item' style={{width}}>
+      {children}
+    </div>
+  )
+}
+
+const Carousel = ({children}) => {
+  return (
+    <div className='carousel'>
+      <div className='inner' style={{transform: "translateX(-0%)"}}>
+        {React.Children.map(children, (child, index) => {
+          return React.cloneElement(child, {width: "100%"})
+        })}
+
+      </div>  
+    </div>
   )
 }
 
--- src/components/customComponents/carousel/CarouselItem.js
+++ src/components/customComponents/carousel/CarouselItem.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+// https://medium.com/tinyso/how-to-create-the-responsive-and-swipeable-carousel-slider-component-in-react-99f433364aa0
+
 const CarouselItem = ({ children,width }) => {
   return (
     <div>CarouselItem</div>
