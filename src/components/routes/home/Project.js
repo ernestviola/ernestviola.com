204eaf71ede2c053b045d3a444a0392cbd29c59e
ernestviola <ernest.viol@gmail.com>
2022-12-20 21:50:46 -0500

    Added indicator buttons

--- src/components/routes/home/Project.js
+++ src/components/routes/home/Project.js
@@ -2,10 +2,18 @@ import React from 'react'
 
 import './styles/Project.css'
 
+import Carousel, {CarouselItem} from '../../customComponents/carousel/Carousel'
+
 const Project = () => {
   return (
     <div className='projectcontainer'>
         <h1>Projects</h1>
+        <Carousel > 
+          <CarouselItem>Item 1</CarouselItem>  
+          <CarouselItem>Item 2</CarouselItem>  
+          <CarouselItem>Item 3</CarouselItem>  
+        </Carousel >
+
 
         
     </div>
