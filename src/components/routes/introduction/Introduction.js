e880f59c77a6bdd88bc2c0f5c3692e95e59fe20a
ernestviola <ernest.viol@gmail.com>
2023-02-22 16:45:30 -0500

    Added timeout transition

--- src/components/routes/introduction/Introduction.js
+++ src/components/routes/introduction/Introduction.js
@@ -7,18 +7,30 @@ const Introduction = () => {
     `Hi, I'm Ernest.`,
     `I'm a software engineer.`
   ]
+  const answerLoop = [
+    `Hi 👋`,
+    'What have you worked on?'
+  ]
   const [count, setCount] = useState(0)
+  const [visible,setVisible] = useState(1)
   // const [introText,setIntroText] = useState(loop[0])
 
   const click = () => {
-    setCount(count + 1)
+    setVisible(0)
+    
+    
+
+    setTimeout(() => {
+      setCount(count + 1)
+      setVisible(1)
+    },500)
   }
 
   return (
     <div className='box'>
-      <div className='introduction'>
+      <div className={visible?'fadeIn introduction':'fadeOut introduction'}>
         <h1 className='question'>{introductionLoop[count % introductionLoop.length]}</h1>
-        <button onClick={click} className='answer'>Hi</button>
+        <button onClick={click} className='answer'>{answerLoop[count % introductionLoop.length]}</button>
       </div>
     </div>
   )
