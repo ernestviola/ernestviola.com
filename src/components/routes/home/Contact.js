ce63f869f62be7faf631896461705c5a71f6c53f
ernestviola <ernest.viol@gmail.com>
2023-02-25 16:32:11 -0800

    Changed page margin to padding

--- src/components/routes/home/Contact.js
+++ src/components/routes/home/Contact.js
@@ -1,29 +1,30 @@
 import React, { useState } from 'react'
 
+import './Contact.css'
+
 const Contact = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
     const [message, setMessage] = useState('')
+
+
     return (
-        <div>
-            <h1>Get in Touch!</h1>
-            <form>
-                <div>
-                    <label>
-                        Name:
-                        <br />
-                        <input type='text' name='name' />
-                    </label>
-                    <label>
-                        Email Address:
-                        <br />
-                        <input type='text' name='email' />
-                    </label>
-                    <label>Message:
-                        <textarea name='message' value={message} onChange={(e) => { setMessage(e.target.value) }}></textarea>
-                    </label>
+        <form className='formGroup page'>
+            <h1>Send a message!</h1>
+            <div className='inputSection'>
+                <div className='inputSection1'>
+                    <input placeholder='Name' type='text' name='name' value={name} onChange={(e) => { setName(e.target.value) }} />
+                    <input placeholder='Email Address' type='text' name='email' value={email} onChange={(e) => { setEmail(e.target.value) }} />
                 </div>
-                <input type='submit' value='Submit' />
-            </form>
-        </div>
+                <div className='inputSection2'>
+                    <textarea placeholder='Hi Ernest!' name='message' value={message} onChange={(e) => { setMessage(e.target.value) }}></textarea>
+                </div>
+            </div>
+            <div className='submitSection'>
+                <input className='submit' type='submit' value='Submit' />
+            </div>
+
+        </form>
     )
 }
 
