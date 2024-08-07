633e229fc1aa9b424367a9e1206272522357fc2c
Ernest V <ernest.viol@gmail.com>
2024-08-07 16:43:13 -0700

    Added tailwind for css

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
 export default function Home() {
-    return <h1>Welcome to my portfolio</h1>
-  }
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold underline">
+        Welcome to my portfolio
+      </h1>
+    </div>
+  )
+}
